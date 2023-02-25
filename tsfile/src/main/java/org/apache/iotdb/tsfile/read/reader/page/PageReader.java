81deb672b418faddf642678378dc6b48ad974c8a
Lei Rui <1010953854@qq.com>
2023-02-25 22:23:07 +0800

    fix

--- tsfile/src/main/java/org/apache/iotdb/tsfile/read/reader/page/PageReader.java
+++ tsfile/src/main/java/org/apache/iotdb/tsfile/read/reader/page/PageReader.java
@@ -18,13 +18,9 @@
  */
 package org.apache.iotdb.tsfile.read.reader.page;
 
-import org.apache.iotdb.tsfile.common.conf.TSFileDescriptor;
 import org.apache.iotdb.tsfile.encoding.decoder.Decoder;
-import org.apache.iotdb.tsfile.encoding.decoder.DeltaBinaryDecoder.LongDeltaDecoder;
-import org.apache.iotdb.tsfile.exception.write.UnSupportedDataTypeException;
 import org.apache.iotdb.tsfile.file.header.PageHeader;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
 import org.apache.iotdb.tsfile.file.metadata.statistics.Statistics;
 import org.apache.iotdb.tsfile.file.metadata.statistics.StepRegress;
 import org.apache.iotdb.tsfile.read.common.BatchData;
@@ -37,7 +33,6 @@ import org.apache.iotdb.tsfile.read.common.block.column.TimeColumnBuilder;
 import org.apache.iotdb.tsfile.read.filter.basic.Filter;
 import org.apache.iotdb.tsfile.read.filter.operator.AndFilter;
 import org.apache.iotdb.tsfile.read.reader.IPageReader;
-import org.apache.iotdb.tsfile.utils.Binary;
 import org.apache.iotdb.tsfile.utils.ReadWriteForEncodingUtils;
 
 import java.io.IOException;
@@ -183,93 +178,171 @@ public class PageReader implements IPageReader {
     return findTheClosetPointEqualOrAfter((long) targetTimestamp, ascending);
   }
 
-  @Override
-  public TsBlock getAllSatisfiedData() throws IOException {
+  /**
+   * Find the point with the closet timestamp equal to or larger than the given timestamp in the
+   * chunk.
+   *
+   * @param targetTimestamp must be within the chunk time range [startTime, endTime]
+   */
+  public TsBlock findTheClosetPointEqualOrAfter_TSBlock(long targetTimestamp) throws IOException {
     TsBlockBuilder builder = new TsBlockBuilder(Collections.singletonList(dataType));
     TimeColumnBuilder timeBuilder = builder.getTimeColumnBuilder();
     ColumnBuilder valueBuilder = builder.getColumnBuilder(0);
-    if (filter == null || filter.satisfy(getStatistics())) {
-      switch (dataType) {
-        case BOOLEAN:
-          while (timeDecoder.hasNext(timeBuffer)) {
-            long timestamp = timeDecoder.readLong(timeBuffer);
-            boolean aBoolean = valueDecoder.readBoolean(valueBuffer);
-            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, aBoolean))) {
-              timeBuilder.writeLong(timestamp);
-              valueBuilder.writeBoolean(aBoolean);
-              builder.declarePosition();
-            }
-          }
-          break;
-        case INT32:
-          while (timeDecoder.hasNext(timeBuffer)) {
-            long timestamp = timeDecoder.readLong(timeBuffer);
-            int anInt = valueDecoder.readInt(valueBuffer);
-            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, anInt))) {
-              timeBuilder.writeLong(timestamp);
-              valueBuilder.writeInt(anInt);
-              builder.declarePosition();
-            }
-          }
-          break;
-        case INT64:
-          while (timeDecoder.hasNext(timeBuffer)) {
-            long timestamp = timeDecoder.readLong(timeBuffer);
-            long aLong = valueDecoder.readLong(valueBuffer);
-            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, aLong))) {
-              timeBuilder.writeLong(timestamp);
-              valueBuilder.writeLong(aLong);
-              builder.declarePosition();
-            }
-          }
-          break;
-        case FLOAT:
-          while (timeDecoder.hasNext(timeBuffer)) {
-            long timestamp = timeDecoder.readLong(timeBuffer);
-            float aFloat = valueDecoder.readFloat(valueBuffer);
-            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, aFloat))) {
-              timeBuilder.writeLong(timestamp);
-              valueBuilder.writeFloat(aFloat);
-              builder.declarePosition();
-            }
-          }
-          break;
-        case DOUBLE:
-          while (timeDecoder.hasNext(timeBuffer)) {
-            long timestamp = timeDecoder.readLong(timeBuffer);
-            double aDouble = valueDecoder.readDouble(valueBuffer);
-            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, aDouble))) {
-              timeBuilder.writeLong(timestamp);
-              valueBuilder.writeDouble(aDouble);
-              builder.declarePosition();
-            }
-          }
-          break;
-        case TEXT:
-          while (timeDecoder.hasNext(timeBuffer)) {
-            long timestamp = timeDecoder.readLong(timeBuffer);
-            Binary aBinary = valueDecoder.readBinary(valueBuffer);
-            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, aBinary))) {
-              timeBuilder.writeLong(timestamp);
-              valueBuilder.writeBinary(aBinary);
-              builder.declarePosition();
-            }
-          }
-          break;
-        default:
-          throw new UnSupportedDataTypeException(String.valueOf(dataType));
+
+    StepRegress stepRegress = pageHeader.getStatistics().getStepRegress();
+    // infer position starts from 1, so minus 1 here
+    int estimatedPos = (int) Math.round(stepRegress.infer(targetTimestamp)) - 1;
+
+    // search from estimatePos in the timeBuffer to find the closet timestamp equal to or larger
+    // than the given timestamp
+    if (timeBuffer.getLong(estimatedPos * 8) < targetTimestamp) {
+      while (timeBuffer.getLong(estimatedPos * 8) < targetTimestamp) {
+        estimatedPos++;
+        // IOMonitor.incPointsTravered();
       }
+    } else if (timeBuffer.getLong(estimatedPos * 8) > targetTimestamp) {
+      while (timeBuffer.getLong(estimatedPos * 8) > targetTimestamp) {
+        estimatedPos--;
+        // IOMonitor.incPointsTravered();
+      }
+      if (timeBuffer.getLong(estimatedPos * 8) < targetTimestamp) {
+        estimatedPos++;
+        // IOMonitor.incPointsTravered();
+      } // else equal
+    } // else equal
+
+    // since we have constrained that targetTimestamp must be within the chunk time range
+    // [startTime, endTime],
+    // we can definitely find such a point with the closet timestamp equal to or larger than the
+    // given timestamp in the chunk.
+    long timestamp = timeBuffer.getLong(estimatedPos * 8);
+    switch (dataType) {
+        // iotdb的int类型的plain编码用的是自制的不支持random access（DOUBLE的plain似乎也有问题）
+        //      case INT32:
+        //        return new MinMaxInfo(pageReader.valueBuffer.getInt(estimatedPos * 4),
+        //            pageReader.timeBuffer.getLong(estimatedPos * 8));
+      case INT64:
+        long longVal = valueBuffer.getLong(timeBufferLength + estimatedPos * 8);
+        timeBuilder.writeLong(timestamp);
+        valueBuilder.writeLong(longVal);
+        builder.declarePosition();
+        break;
+        //      case FLOAT:
+        //        float floatVal = valueBuffer.getFloat(timeBufferLength + estimatedPos * 4);
+        //        timeBuilder.writeLong(timestamp);
+        //        valueBuilder.writeFloat(floatVal);
+        //        builder.declarePosition();
+        //        break;
+        //      case DOUBLE:
+        //        double doubleVal = valueBuffer.getDouble(timeBufferLength + estimatedPos * 8);
+        //        timeBuilder.writeLong(timestamp);
+        //        valueBuilder.writeDouble(doubleVal);
+        //        builder.declarePosition();
+        //        break;
+      default:
+        throw new IOException("Unsupported data type!");
     }
-    if (TSEncoding.valueOf(TSFileDescriptor.getInstance().getConfig().getTimeEncoder())
-        .equals(TSEncoding.TS_2DIFF)) {
-      loadIntBatch_ns = ((LongDeltaDecoder) timeDecoder).loadIntBatch_ns;
-      loadIntBatch_cnt = ((LongDeltaDecoder) timeDecoder).loadIntBatch_cnt;
-      ((LongDeltaDecoder) timeDecoder).loadIntBatch_ns =
-          0; // reset because timeDecoder is global for all pageReaders of a chunk
-      ((LongDeltaDecoder) timeDecoder).loadIntBatch_cnt =
-          0; // reset because timeDecoder is global for all pageReaders of a chunk
-    }
+
     return builder.build();
+  }
+
+  @Override
+  public TsBlock getAllSatisfiedData() throws IOException {
+    // TODO
+    double targetTimestamp = (pageHeader.getStartTime() + pageHeader.getEndTime()) / 2.0;
+    return findTheClosetPointEqualOrAfter_TSBlock((long) targetTimestamp);
+
+    //    TsBlockBuilder builder = new TsBlockBuilder(Collections.singletonList(dataType));
+    //    TimeColumnBuilder timeBuilder = builder.getTimeColumnBuilder();
+    //    ColumnBuilder valueBuilder = builder.getColumnBuilder(0);
+    //    if (filter == null || filter.satisfy(getStatistics())) {
+    //      switch (dataType) {
+    //        case BOOLEAN:
+    //          while (timeDecoder.hasNext(timeBuffer)) {
+    //            long timestamp = timeDecoder.readLong(timeBuffer);
+    //            boolean aBoolean = valueDecoder.readBoolean(valueBuffer);
+    //            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp,
+    // aBoolean))) {
+    //              timeBuilder.writeLong(timestamp);
+    //              valueBuilder.writeBoolean(aBoolean);
+    //              builder.declarePosition();
+    //            }
+    //          }
+    //          break;
+    //        case INT32:
+    //          while (timeDecoder.hasNext(timeBuffer)) {
+    //            long timestamp = timeDecoder.readLong(timeBuffer);
+    //            int anInt = valueDecoder.readInt(valueBuffer);
+    //            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, anInt)))
+    // {
+    //              timeBuilder.writeLong(timestamp);
+    //              valueBuilder.writeInt(anInt);
+    //              builder.declarePosition();
+    //            }
+    //          }
+    //          break;
+    //        case INT64:
+    //          while (timeDecoder.hasNext(timeBuffer)) {
+    //            long timestamp = timeDecoder.readLong(timeBuffer);
+    //            long aLong = valueDecoder.readLong(valueBuffer);
+    //            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp, aLong)))
+    // {
+    //              timeBuilder.writeLong(timestamp);
+    //              valueBuilder.writeLong(aLong);
+    //              builder.declarePosition();
+    //            }
+    //          }
+    //          break;
+    //        case FLOAT:
+    //          while (timeDecoder.hasNext(timeBuffer)) {
+    //            long timestamp = timeDecoder.readLong(timeBuffer);
+    //            float aFloat = valueDecoder.readFloat(valueBuffer);
+    //            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp,
+    // aFloat))) {
+    //              timeBuilder.writeLong(timestamp);
+    //              valueBuilder.writeFloat(aFloat);
+    //              builder.declarePosition();
+    //            }
+    //          }
+    //          break;
+    //        case DOUBLE:
+    //          while (timeDecoder.hasNext(timeBuffer)) {
+    //            long timestamp = timeDecoder.readLong(timeBuffer);
+    //            double aDouble = valueDecoder.readDouble(valueBuffer);
+    //            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp,
+    // aDouble))) {
+    //              timeBuilder.writeLong(timestamp);
+    //              valueBuilder.writeDouble(aDouble);
+    //              builder.declarePosition();
+    //            }
+    //          }
+    //          break;
+    //        case TEXT:
+    //          while (timeDecoder.hasNext(timeBuffer)) {
+    //            long timestamp = timeDecoder.readLong(timeBuffer);
+    //            Binary aBinary = valueDecoder.readBinary(valueBuffer);
+    //            if (!isDeleted(timestamp) && (filter == null || filter.satisfy(timestamp,
+    // aBinary))) {
+    //              timeBuilder.writeLong(timestamp);
+    //              valueBuilder.writeBinary(aBinary);
+    //              builder.declarePosition();
+    //            }
+    //          }
+    //          break;
+    //        default:
+    //          throw new UnSupportedDataTypeException(String.valueOf(dataType));
+    //      }
+    //    }
+    //    if (TSEncoding.valueOf(TSFileDescriptor.getInstance().getConfig().getTimeEncoder())
+    //        .equals(TSEncoding.TS_2DIFF)) {
+    //      loadIntBatch_ns = ((LongDeltaDecoder) timeDecoder).loadIntBatch_ns;
+    //      loadIntBatch_cnt = ((LongDeltaDecoder) timeDecoder).loadIntBatch_cnt;
+    //      ((LongDeltaDecoder) timeDecoder).loadIntBatch_ns =
+    //          0; // reset because timeDecoder is global for all pageReaders of a chunk
+    //      ((LongDeltaDecoder) timeDecoder).loadIntBatch_cnt =
+    //          0; // reset because timeDecoder is global for all pageReaders of a chunk
+    //    }
+    //    return builder.build();
   }
 
   @Override
